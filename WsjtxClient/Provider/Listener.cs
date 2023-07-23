ee4a0461bc0875d8fd2740f39a9bb26068dcb29d
Jeff Uchitjil <git@k9kld.org>
2023-07-23 09:00:00 -0500

    Add option to pass specific listener config

---
+++ WsjtxClient/Provider/Listener.cs
@@ -0,0 +1,10 @@
+namespace WsjtxClient.Provider;
+
+public class ListenerConfig
+{
+    public string Ip { get; set; }
+
+    public int Port { get; set; }
+
+    public bool Multicast { get; set; }
+}
